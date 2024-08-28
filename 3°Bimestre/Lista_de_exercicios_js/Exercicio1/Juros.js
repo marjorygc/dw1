30803dbe9be56b25a6cfc3d20106a2268d48fc26
marjorygc <163425550+marjorygc@users.noreply.github.com>
2024-08-28 17:16:21 -0300

    exercicio1
    Arrumar calculo
    Centralizar conteúdo

---
+++ 3°Bimestre/Lista_de_exercicios_js/Exercicio1/Juros.js
@@ -0,0 +1,22 @@
+let inputCapital = document.getElementById("capital")
+let inputTaixa = document.getElementById("taixa")
+let inputPeriodo = document.getElementById("periodo")
+let divSaida = document.getElementById("saida")
+
+let botaoCalcular = document.getElementById("calcular")
+// vincula a função ao clique do botão
+botaoCalcular.onclick = calculaJuros
+
+function calculaJuros(){
+    alert("Calculando")
+    // Entrada       
+    let capital = Number(inputCapital.value)
+    let taixa = Number(inputTaixa.value)
+    let periodo = Number(inputPeriodo)
+
+    // Processamento
+    let m = capital * (1+taixa)^periodo
+
+    // Saída
+    divSaida.innerText = m
+}
