b478965a08f5fbccaa58b5b9b8313de0c60f9dbe
marjorygc <163425550+marjorygc@users.noreply.github.com>
2024-09-09 11:18:12 -0300

    tarefas quase prontas

--- 3°Bimestre/aula-05-09-2024/multas.js
+++ 3°Bimestre/aula-05-09-2024/multas.js
@@ -8,18 +8,22 @@ let botaoCalcular = document.getElementById("calcular")
 botaoCalcular.onclick = multa
 
 function multa(){
-
+    alert("Calculando")
     let velocidade_maxima = Number(inputVelocidade_maxima.value)
     let velocidade_veiculo = Number(inputVelocidade_veiculo.value)
 
     let multa50 = velocidade_veiculo > (velocidade_maxima + (velocidade_maxima*0.5))
-    let multa20_50 = velocidade_veiculo >= (velocidade_maxima + (velocidade_maxima*0.2) && velocidade_maxima <= (velocidade_maxima*0.5))
+    let multa20_50 = (velocidade_veiculo > (velocidade_maxima + (velocidade_maxima*0.2))) && (velocidade_veiculo <= (velocidade_maxima + (velocidade_maxima*0.5))) 
+    let multa20 = velocidade_veiculo <= (velocidade_maxima + (velocidade_maxima*0.2))
 
-    if(multa50 == True){
+    if(multa50 == true){
         divSaida.innerText = "O valor da sua multa é de R$ 880,41"
-    } else { 
-        if(multa20_50 == True){
-            divSaida.innerText = "O valor da sua multa é de R$ 195,23"
-        }} else {
-            divSaida.innerText = "Você está dentro dos limetes de velocidade permitidos"
-        }}
+    } else if(multa20_50 == true){
+        divSaida.innerText = "O valor da sua multa é de R$ 195,23"
+    }else if(multa20 == true){
+        divSaida.innerText = "O valor da sua multa é de R$ 130,16"
+        }
+    else{
+        divSaida.innerText = "Você está dentro dos limetes de velocidade permitidos"
+    }
+    }   
